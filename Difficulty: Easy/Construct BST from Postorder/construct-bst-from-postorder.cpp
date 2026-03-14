0b7589ce7d053b46e7b0639144106d5ee7986f0f
SAURABH KUMAR <aiml23031@glbitm.ac.in>
2026-03-15 00:01:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Construct BST from Postorder/construct-bst-from-postorder.cpp
@@ -0,0 +1,24 @@
+/*struct Node
+{
+    int data;
+    Node *left, *right;
+};*/
+  
+ Node*  BST(int post[],int& index,int lower,int upper)
+ {
+     if(index<0 || post[index]<lower || post[index]>upper)
+     {
+         return NULL;
+     }
+     
+     Node* root=new Node(post[index--]);
+     root->right=BST(post,index,root->data,upper);
+     root->left=BST(post,index,lower,root->data);
+     
+     return root;
+ }
+Node *constructTree(int post[], int size) {
+    // code here
+    int index=size-1;
+    return BST(post,index,INT_MIN,INT_MAX);
+}
