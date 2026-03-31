543967f7c0a655e0a9677487c39549b298f1e2e8
SAURABH KUMAR <aiml23031@glbitm.ac.in>
2026-03-31 23:56:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Flattening a Linked List/flattening-a-linked-list.cpp
@@ -0,0 +1,67 @@
+/*
+class Node {
+public:
+    int data;
+    Node* next;
+    Node* bottom;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+        bottom = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+  Node *merge(Node* head1,Node* head2)
+  {
+      Node *head=new Node(0);
+      Node *tail=head;
+      
+      //
+      while(head1 &&head2)
+      {
+          if(head1->data<=head2->data)
+          {
+              tail->bottom=head1;
+              head1=head1->bottom;
+              tail=tail->bottom;
+              tail->bottom=NULL;
+          }
+          else
+          {
+              tail->bottom=head2;
+              head2=head2->bottom;
+              tail=tail->bottom;
+              tail->bottom=NULL;
+          }
+      }
+      if(head1)
+      tail->bottom=head1;
+      else
+      tail->bottom=head2;
+      
+      return head->bottom;
+  }
+    Node *flatten(Node *root) {
+        // code here
+        Node *head1,*head2,*head3;
+        
+        while(root->next)
+        {
+            head1=root;
+            head2=root->next;
+            head3=root->next->next;
+            
+            head1->next=NULL;
+            head2->next=NULL;
+            
+            root=merge(head1,head2);
+            root->next=head3;
+            
+        }
+        return root;
+    }
+};
